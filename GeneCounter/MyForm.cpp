0067418ef3e4bba7389d786c46aad78ff28a0219
Sergio Vacas <vaks@hotmail.es>
2018-04-14 19:10:50 +0200

    change img working

--- GeneCounter/MyForm.cpp
+++ GeneCounter/MyForm.cpp
@@ -1,10 +1,25 @@
 #include "MyForm.h"
 
+#include "opencv2\opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "iostream"
+
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
+using namespace cv;
+using namespace std;
+
 using namespace System;
 using namespace System::Windows::Forms;
+using namespace System::Drawing;
 
 [STAThread]
-int main(array<String^>^ args)
+int main(cli::array<System::String^>^ args)
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
@@ -12,4 +27,3 @@ int main(array<String^>^ args)
 	Application::Run(%form);
 	return 0;
 }
-
--- GeneCounter/MyForm.h
+++ GeneCounter/MyForm.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "opencv2\opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "iostream"
 
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
+using namespace cv;
+using namespace std;
 namespace GeneCounter {
 
 	using namespace System;
@@ -37,6 +49,8 @@ namespace GeneCounter {
 	private: System::Windows::Forms::Label^  lName;
 	private: System::Windows::Forms::TextBox^  txtBoxName;
 	private: System::Windows::Forms::Button^  btnSiembralo;
+	private: System::Windows::Forms::PictureBox^  imgBox;
+	private: System::Windows::Forms::TrackBar^  trackBarPercentil;
 	protected:
 
 	protected:
@@ -54,15 +68,20 @@ namespace GeneCounter {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->lName = (gcnew System::Windows::Forms::Label());
 			this->txtBoxName = (gcnew System::Windows::Forms::TextBox());
 			this->btnSiembralo = (gcnew System::Windows::Forms::Button());
+			this->imgBox = (gcnew System::Windows::Forms::PictureBox());
+			this->trackBarPercentil = (gcnew System::Windows::Forms::TrackBar());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->imgBox))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBarPercentil))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// lName
 			// 
 			this->lName->AutoSize = true;
-			this->lName->Location = System::Drawing::Point(121, 117);
+			this->lName->Location = System::Drawing::Point(858, 67);
 			this->lName->Name = L"lName";
 			this->lName->Size = System::Drawing::Size(35, 13);
 			this->lName->TabIndex = 0;
@@ -70,14 +89,14 @@ namespace GeneCounter {
 			// 
 			// txtBoxName
 			// 
-			this->txtBoxName->Location = System::Drawing::Point(91, 63);
+			this->txtBoxName->Location = System::Drawing::Point(861, 27);
 			this->txtBoxName->Name = L"txtBoxName";
 			this->txtBoxName->Size = System::Drawing::Size(100, 20);
 			this->txtBoxName->TabIndex = 1;
 			// 
 			// btnSiembralo
 			// 
-			this->btnSiembralo->Location = System::Drawing::Point(101, 165);
+			this->btnSiembralo->Location = System::Drawing::Point(886, 480);
 			this->btnSiembralo->Name = L"btnSiembralo";
 			this->btnSiembralo->Size = System::Drawing::Size(75, 23);
 			this->btnSiembralo->TabIndex = 2;
@@ -85,17 +104,38 @@ namespace GeneCounter {
 			this->btnSiembralo->UseVisualStyleBackColor = true;
 			this->btnSiembralo->Click += gcnew System::EventHandler(this, &MyForm::btnSiembralo_Click);
 			// 
+			// imgBox
+			// 
+			this->imgBox->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"imgBox.Image")));
+			this->imgBox->Location = System::Drawing::Point(12, 12);
+			this->imgBox->Name = L"imgBox";
+			this->imgBox->Size = System::Drawing::Size(693, 414);
+			this->imgBox->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->imgBox->TabIndex = 3;
+			this->imgBox->TabStop = false;
+			// 
+			// trackBarPercentil
+			// 
+			this->trackBarPercentil->Location = System::Drawing::Point(824, 353);
+			this->trackBarPercentil->Name = L"trackBarPercentil";
+			this->trackBarPercentil->Size = System::Drawing::Size(104, 45);
+			this->trackBarPercentil->TabIndex = 4;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(973, 515);
+			this->Controls->Add(this->trackBarPercentil);
+			this->Controls->Add(this->imgBox);
 			this->Controls->Add(this->btnSiembralo);
 			this->Controls->Add(this->txtBoxName);
 			this->Controls->Add(this->lName);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->imgBox))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBarPercentil))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -105,6 +145,24 @@ namespace GeneCounter {
 	}
 	private: System::Void btnSiembralo_Click(System::Object^  sender, System::EventArgs^  e) {
 		lName->Text = txtBoxName->Text;
+		Mat img = imread("./img/4.bmp", CV_LOAD_IMAGE_COLOR);
+		showImage(img);
+		
+	}
+	void showImage(Mat img) {
+		imwrite("./img/temp/temp.bmp", img);
+		imgBox->Image = System::Drawing::Image::FromFile("./img/temp/temp.bmp");
+		/*
+		imgBox->Image = gcnew Bitmap(img.size().width,
+									 img.size().height,
+									 img.step,
+									 Imaging::PixelFormat::Format24bppRgb,
+									 (IntPtr)img.data);
+		*/
+	}
+	void saveImage(string name,Mat img) {
+		imwrite("./img/temp/"+name+".bmp", img);
 	}
 	};
+	
 }
